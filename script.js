0e5dd54b52c479bbb4db80431c8e0af24565c032
Manikandan <mani18012003@gmail.com>
2025-06-30 19:06:41 +0530

    upto home

--- script.js
+++ script.js
@@ -39,80 +39,34 @@ if (savedTheme) {
   applyTheme(savedTheme);
 }
 
-// Slider
-const slides = document.querySelectorAll(".slide");
-const sliderNav = document.querySelector(".slider-nav");
-const slider = document.querySelector(".slider");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
 
-let currentSlide = 0;
-let slideCount = slides.length;
-let intervalId = null;
+//Animate Text
+const txts = document.querySelector(".head").children,
+      txtsLen = txts.length;
+let index=0;
+const textInTimer = 1500,
+      textOutTimer = 1300;
 
-slides.forEach((_, index) => {
-  const dot = document.createElement("div");
-  dot.classList.add("nav-dot");
-
-  if (index === 0) dot.classList.add("active");
-
-  dot.addEventListener("click", () => {
-    goToSlide(index);
-  });
-
-  sliderNav.appendChild(dot);
-});
-
-const dots = document.querySelectorAll(".nav-dot");
-
-function updateDots() {
-  dots.forEach((dot, index) => {
-    dot.classList.toggle("active", index === currentSlide);
-  });
-}
-
-function goToSlide(index) {
-  currentSlide = (index + slideCount) % slideCount;
-  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
-  resetAnimation(slides[currentSlide]);
-  updateDots();
-}
-
-function resetAnimation(slide) {
-  const content = slide.querySelector(".slide-content");
-  if (content) {
-    const clone = content.cloneNode(true);
-    content.parentNode.replaceChild(clone, content);
+function animateText() {
+  for(let i=0; i<txtsLen; i++) {
+    txts[i].classList.remove("text-in", "text-out");
   }
-}
+  txts[index].classList.add("text-in");
+  
+  setTimeout(function() {
+    txts[index].classList.add("text-out");
+  }, textOutTimer);
 
-function startAutoAdvance() {
-  if (!intervalId) {
-    intervalId = setInterval(handleNextSlide, 5000);
-  }
-}
+  setTimeout(function() {
+    if(index == txtsLen-1) {
+    index=0;
+    }
+    else{
+      index++;
+    }
+    animateText();
+  }, textInTimer);
 
-function stopAutoAdvance() {
-  clearInterval(intervalId);
-  intervalId = null;
-}
+} 
 
-startAutoAdvance();
-
-slider.addEventListener("mouseenter", stopAutoAdvance);
-slider.addEventListener("mouseleave", startAutoAdvance);
-
-function handleNextSlide() {
-  stopAutoAdvance();
-  goToSlide(currentSlide + 1);
-  startAutoAdvance();
-}
-
-function handlePrevSlide() {
-  stopAutoAdvance();
-  goToSlide(currentSlide - 1);
-  startAutoAdvance();
-}
-
-prevBtn.addEventListener("click", handlePrevSlide);
-nextBtn.addEventListener("click", handleNextSlide);
+window.onload = animateText;
